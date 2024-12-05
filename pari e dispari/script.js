a827267be0534ec5f50a2b3674369db072356e31
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-05 11:03:20 +0100

    pari e dispari done

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -3,21 +3,53 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
-const evenOrOdd = prompt("inserisci pari o dispari");
-const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
-const cpuNumber = Math.floor(Math.random() * 5) + 1;
-const sum = userNumber + cpuNumber;
-console.log(`La cpu a scelto: ${cpuNumber}`)
-let resultEvenOrOdd = "";
-if (sum % 2 === 0) {
-    resultEvenOrOdd = "pari";
-} else {
-    resultEvenOrOdd = "dispari";
+
+/**
+ * return a random number between min and max (both included)
+ * @param {number} min - min number
+ * @param {number} max - max number
+ * @returns {number}
+ */
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (min -max + 1)) + min;
+}
+
+/**
+ * check if a number is even
+ * @param {number} num
+ * @returns {boolean}
+ */
+function isEven(num) {
+    if (sum % 2 === 0) {
+        return true;
+    }
+    return false;
 }
 
 
-if(resultEvenOrOdd === evenOrOdd) {
+//PROGRAMMA PRINCIPALE
+const userEvenOrOdd = prompt("inserisci pari o dispari");
+const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
+const cpuNumber = Math.floor(Math.random() * 5) + 1;
+const sum = userNumber + cpuNumber;
+
+// console.log(`La cpu a scelto: ${cpuNumber}`)
+// let resultEvenOrOdd = "";
+// if (sum % 2 === 0) {
+//     resultEvenOrOdd = "pari";
+// } else {
+//     resultEvenOrOdd = "dispari";
+// }
+
+// if(resultEvenOrOdd === userEvenOrOdd) {
+//     alert("Hai vinto");
+// } else {
+//     alert("Hai vinto");
+// }
+
+if ((isEven(num) && userEvenOrOdd === 'pari') || (!isEven(num) % 2 === 1 && userEvenOrOdd === 'dispari')) {
     alert("Hai vinto");
 } else {
-    alert("Hai vinto");
+    alert("Hai perso")
 }
