5017251028dd448d631f36ee2630962e1246ea8a
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-05 10:13:32 +0100

    palindrome  exc

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,2 +1,15 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+const userWord = prompt('inserisci una parola');
+let reverseWord = "";
+
+for (let i = userWord.length - 1; i >= 0; i--) {
+    reverseWord = reverseWord + userWord[i];
+}
+
+if(userWord === reverseWord) {
+    alert('la parola è palindroma')
+} else {
+    alert('la parola non è palindroma')
+}
+
